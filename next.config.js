de5504e2c76a918525043ab7c0de72885676fd53
Zymate Studio <77316475+SaiKiranSparkApps@users.noreply.github.com>
2023-06-01 12:26:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
 				]	
 			}, 
 			{ 
-				source: '/progress/:id', 
+				source: '/progress/:id*', 
 				headers: [
 					{ 
 						key: 'Access-Control-Allow-Origin', 
@@ -44,7 +44,7 @@ const nextConfig = {
 				]
 			},
 			{ 
-				source: '/tasks/:id', 
+				source: '/tasks/:id*', 
 				headers: [
 					{ 
 						key: 'Access-Control-Allow-Origin', 
